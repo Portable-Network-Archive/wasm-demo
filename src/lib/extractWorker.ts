cf1f411ec4afcab0fdb912e5ed9cf8a08024845d
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2024-01-07 11:49:23 +0900

    :sparkles: Support extract encrypted archive

--- src/lib/extractWorker.ts
+++ src/lib/extractWorker.ts
@@ -1,12 +1,26 @@
 const workder = self;
-workder.addEventListener("message", async (e: MessageEvent<File>) => {
-  const pna = await import("pna");
-  await pna.default();
-  let entries = await pna.Archive.extract_to_entries(e.data);
-  entries.array().map(async (entry, idx) => {
-    const path = entry.name();
-    const data = await entry.extract();
-    const file = new File([data], path);
-    workder.postMessage([idx, file]);
-  });
-});
+workder.addEventListener(
+  "message",
+  async (e: MessageEvent<[File, string | undefined]>) => {
+    const pna = await import("pna");
+    await pna.default();
+    const [archive, password] = e.data;
+    let entries = await pna.Archive.extract_to_entries(archive);
+    entries.array().map(async (entry, idx) => {
+      const path = entry.name();
+      try {
+        if (entry.is_encrypted()) {
+          const data = await entry.extract(password);
+          const file = new File([data], path);
+          workder.postMessage([idx, file]);
+        } else {
+          const data = await entry.extract();
+          const file = new File([data], path);
+          workder.postMessage([idx, file]);
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    });
+  },
+);
