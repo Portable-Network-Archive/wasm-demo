df3ee44b809c0240d92713a6c7528923ff58892c
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2026-03-21 21:34:31 +0900

    :wrench: Add ignores to ESLint flat config
    Exclude node_modules, out, docs, and pna_wasm/pkg from linting
    to avoid scanning minified/generated files.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,10 @@
 import nextConfig from "eslint-config-next/core-web-vitals";
 import prettierConfig from "eslint-config-prettier";
 
-export default [...nextConfig, prettierConfig];
+export default [
+  {
+    ignores: ["node_modules/", "out/", "docs/", "pna_wasm/pkg/"],
+  },
+  ...nextConfig,
+  prettierConfig,
+];
